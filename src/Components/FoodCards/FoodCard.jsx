1d798a5ee273f99a9ceafab079a5a9b9bb21b9cc
AceHunterr <aggarwalmehul26@gmail.com>
2023-08-27 03:24:05 +0530

    Added cards

---
+++ src/Components/FoodCards/FoodCard.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { FaRegBookmark } from "react-icons/fa";
+import { useNavigate, Link } from "react-router-dom";
+import "./FoodCard.css";
+
+const FoodCard = (props) => {
+  const navigate = useNavigate();
+  const navigatetoJobDetails = () => {
+    navigate("/job-details", { state: { jobProps: props } });
+  };
+
+  const navigatetoTagsPage = () => {
+    navigate("/tags");
+  };
+
+  const customClassName = props.work_type;
+  return (
+    <div className="jobList">
+      <div key={props.id} className={`${customClassName} jobCard`}>
+        <div className="jobCard__content">
+          <div className="jobCard__header" onClick={navigatetoJobDetails}>
+            <img src={props.image} alt="job-img" className="jobImage" />
+            <h3 className="jobName ">{props.job_title}</h3>
+          </div>
+          {/* <FaRegBookmark className={`jobCard__wishlist ${isSaved ? 'saved' : ''}`} onClick={handleBookmarkClick} /> */}
+          <FaRegBookmark className="jobCard__wishlist" />
+          <h3 className="jobCompany">{props.company}</h3>
+          <div className="displayStack__1">
+            <div className="tagsContainer">
+              {props.tags.map((tag, index) => (
+                <span
+                  key={index}
+                  className="tagItem"
+                  onClick={navigatetoTagsPage}
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <div className="displayStack__2">
+              <div className="jobview" onClick={navigatetoJobDetails}>
+                {props.view}
+              </div>
+              <div className="jobview" onClick={navigatetoJobDetails}>
+                {props.share}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
---
+++ src/Pages/FoodCardsPage/FoodCardsPage.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import FoodCard from "../../Components/FoodCards/FoodCard";
+import foodData from "../../Data/FoodContent";
+import { Box } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+const FoodCardsPage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredJobs, setFilteredJobs] = useState(foodData);
+
+  const handleSearch = (event) => {
+    const { value } = event.target;
+    setSearchQuery(value);
+
+    const filtered = foodData.filter((job) => {
+      const titleMatch = job.job_title
+        .toLowerCase()
+        .includes(value.toLowerCase());
+      const companyMatch = job.company
+        .toLowerCase()
+        .includes(value.toLowerCase());
+      const locationMatch = job.location
+        .toLowerCase()
+        .includes(value.toLowerCase());
+      return titleMatch || companyMatch || locationMatch;
+    });
+
+    setFilteredJobs(filtered);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <div>
+          <div className="job-detail-header-div">
+            <h2 className="jobs-text job-detail-heading">Menu Items</h2>
+          </div>
+
+          <div className="search-bar">
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearch}
+              placeholder="Search jobs..."
+            />
+
+            <SearchIcon className="search-icon" />
+          </div>
+          <div
+            className="jobcard-container"
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "50px",
+            }}
+          >
+            {filteredJobs.map((contents) => (
+              <FoodCard
+                key={contents.id}
+                image={contents.image}
+                job_title={contents.job_title}
+                company={contents.company}
+                view={contents.view}
+                share={contents.share}
+                tags={contents.tags}
+                description={contents.description}
+                mode={contents.mode}
+                location={contents.location}
+                days={contents.date_posted}
+                salary={contents.salary}
+                job_type={contents.job_type}
+                position={contents.position}
+                work_type="work_type_job"
+                // onSaveCard={handleSaveCard}
+                style={{ flex: "1 10 50%", maxWidth: "50%" }}
+              />
+            ))}
+          </div>
+
+          {/* <FoodCard /> */}
+        </div>
+      </Box>
+    </Box>
+  );
+};
+
+export default FoodCardsPage;
